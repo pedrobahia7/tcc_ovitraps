17debf59333c2877d0e39edc1a31ed226c679ada
pedro-aignosi <pedro.bahia@aignosi.com>
2024-11-06 15:37:20 -0300

    clean pipelines

--- src/NN.py
+++ src/NN.py
@@ -64,17 +64,11 @@ def NN_pipeline(parameters:dict, data_path:str= None)->None:
 
     # Network structure
     model_input, model_output = NN_building.input_output_sizes(lags, ntraps, use_trap_info,model_type,input_3d)
-    if model_type == 'logistical' or model_type == 'linear_regressor':
-        model = NN_arquitectures.LogisticRegression(model_input,input_3d, model_type).to(device)
-
-    elif model_type == 'exponential_renato':
-        model = NN_arquitectures.NeuralNetworkExponential(model_input, model_output,model_type,input_3d).to(device)
-
-    else:
-        model = NN_arquitectures.NeuralNetwork(model_input, model_output,model_type,input_3d).to(device)
+    model = NN_building.define_model(model_type, model_input, model_output, input_3d,device)
 
     # Loss functions
     loss_func_class, loss_func_reg = NN_building.define_loss_functions(model_type)
+    
     # Optimizer
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
@@ -87,17 +81,13 @@ def NN_pipeline(parameters:dict, data_path:str= None)->None:
     # Network Loop
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
-        total_loss_train, loss_class_train, loss_reg_train, acc_class_train, acc_reg_train, error_reg_train = NN_building.train_loop(train_dataloader,
-                                                                              model, loss_func_class, loss_func_reg, optimizer)        
+        results_train = NN_building.train_loop(train_dataloader, model, loss_func_class, loss_func_reg, optimizer)        
         
-        total_loss_test, loss_class_test, loss_reg_test, acc_class_test, acc_reg_test, error_reg_test = NN_building.test_loop(test_dataloader,
-                                                                                        model, loss_func_class, loss_func_reg)
+        results_test = NN_building.test_loop(test_dataloader,model, loss_func_class, loss_func_reg)
 
         # Append Metrics
-        NN_building.append_history_dict(train_history, total_loss=total_loss_train, loss_class=loss_class_train, loss_reg=loss_reg_train, 
-                            acc_class=acc_class_train, acc_reg=acc_reg_train, error_reg=error_reg_train)
-        NN_building.append_history_dict(test_history, total_loss=total_loss_test, loss_class=loss_class_test, loss_reg=loss_reg_test, 
-                            acc_class=acc_class_test, acc_reg=acc_reg_test, error_reg=error_reg_test)
+        NN_building.append_history_dict(train_history, results_train)
+        NN_building.append_history_dict(test_history, results_test)
         
         torch.save(model.state_dict(), f'./results/NN/save_parameters/model{model_type}_lags{lags}_ntraps{ntraps}_epoch{t}.pth')
 
@@ -121,13 +111,13 @@ if __name__ == '__main__':
     '''
     # Parameters
 
-    repeat = 1 # Number of times the model will be trained and tested
+    repeat = 3 # Number of times the model will be trained and tested
 
-    model_type =  'classifier' # 'classifier' or 'regressor' or 'exponential_renato' or 'linear_regressor' or 'logistical'
+    models = ['pareto' ,'exponential_renato' ,'regressor'  ,'logistical' ,'linear_regressor' ]  # 'classifier' or 'regressor' or 'exponential_renato' or 'linear_regressor' or 'logistical'
     use_trap_info = True
-    ntraps = 1
+    neigh_num = [3,5,10]
     lags = 3
-    random_split = True
+    random_split = False
     test_size = 0.2
     scale = False
     learning_rate =1e-3
@@ -136,9 +126,9 @@ if __name__ == '__main__':
     input_3d = False
 
     parameters = {
-        'model_type': model_type,
+        'model_type': [],
         'use_trap_info': use_trap_info,
-        'ntraps': ntraps,
+        'ntraps': [ ],
         'lags': lags,
         'random_split': random_split,
         'test_size': test_size,
@@ -149,8 +139,9 @@ if __name__ == '__main__':
         'input_3d': input_3d  
         }
 
-    metatron = ['classifier' ,'regressor' ,'exponential_renato' ,'linear_regressor' ,'logistical']
+
     bool_list = [True, False]
+   
 
     """    for boolean1 in bool_list:
         parameters['use_trap_info'] = boolean1
@@ -158,5 +149,10 @@ if __name__ == '__main__':
             parameters['random_split'] = boolean2
             for type in metatron:
                 parameters['model_type'] = type
-                for i in range(repeat):"""
-    NN_pipeline(parameters)
+        """
+    for i in range(repeat):
+        for model in models:
+            parameters['model_type'] = model
+            for ntraps in neigh_num:
+                parameters['ntraps'] = ntraps
+                NN_pipeline(parameters)
